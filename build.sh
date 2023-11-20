1b517a9265f1aba37e7e30c3be113b00c1c5798e
Vitaliy Lytonin <mstrom2@rambler.ru>
2023-11-20 22:55:54 +0400

    improved build.sh main script to get possibility to split build process

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 set -e
 set -x
 
-# Read config.ini
-while IFS= read -r line; do line=$(echo "$line" | tr -d '\r'); export "$line"; done < "./config.ini"
-
 ##### script invoke examples: #####
 # ./build.sh \
 #     OS=Linux \
@@ -22,6 +19,8 @@ while IFS= read -r line; do line=$(echo "$line" | tr -d '\r'); export "$line"; d
 
 
 ##### required environment or commandline variables: ######
+# export STAGE=Build          # Config, Build, Deploy. If not specified, all three stages are implied
+
 # export OS=Android          # Android, Linux, Windows
 # export BUILD_TYPE=Debug    # Debug, Release
 # export Qt6_DIR=~/Qt/6.5.1/gcc_64  # Qt for target platform
@@ -33,7 +32,7 @@ export ABI=x86_64       # armv8, armv7, x86_64, x86
 # export ANDROID_DEVICE_ID=$(adb devices | sed -n 2p | awk '{print $1}')
 
 
-# Create commandline-based local variables
+# Create variables from commandline params
 for param in "$@"
 do
     IFS='=' read -r -a param_parts <<< "$param"
@@ -52,125 +51,129 @@ check_variable_existence() {
 }
 
 check_variable_existence Qt6_DIR
-check_variable_existence PROJECT_NAME
-check_variable_existence PROJECT_VERSION
 check_variable_existence OS
 check_variable_existence ABI
 check_variable_existence BUILD_TYPE
 
-rm -rf build CMakePresets.json CMakeUserPresets.json
-DESTINATION_DIR=./build/$OS/$ABI/$BUILD_TYPE
-mkdir -p $DESTINATION_DIR
-
-declare -A CONAN_PRESET
-CONAN_PRESET["Debug"]="conan-debug"
-CONAN_PRESET["Release"]="conan-release"
-
-
-# Translations
 if [ "$OS" == "Android" ]; then
     export QT_HOST_PATH=${Qt6_DIR}/../gcc_64
 else
     export QT_HOST_PATH=$Qt6_DIR
 fi
 
-while IFS= read -r lang
-do
-    lang=$(echo "$lang" | tr -d '\r')
-    ${QT_HOST_PATH}/bin/lupdate ./src/ -ts ./translations/translation_${lang}.ts
-    ${QT_HOST_PATH}/bin/lrelease ./translations/*.ts
-done < "./translations/list.txt"
-mkdir -p ./rcc/rcc
-mv ./translations/*.qm ./rcc/rcc
+# Read variables from config.ini
+while IFS= read -r line; do line=$(echo "$line" | tr -d '\r'); export "$line"; done < "./config.ini"
+check_variable_existence PROJECT_NAME
+check_variable_existence PROJECT_VERSION
+
+DESTINATION_DIR=./build/$OS/$ABI/$BUILD_TYPE
+mkdir -p $DESTINATION_DIR
 
 
-# Resources
-./rcc/rcc.sh
+# Config
+if ! [[ -n "$STAGE" ]] || [[ "$STAGE" == "Config" ]]; then
+    rm -rf $DESTINATION_DIR/* CMakePresets.json CMakeUserPresets.json
+    declare -A CONAN_PRESET
+    CONAN_PRESET["Debug"]="conan-debug"
+    CONAN_PRESET["Release"]="conan-release"
+
+    conan profile detect -f
+    case $OS in
+        "Linux")
+            conan install . \
+                -s build_type=$BUILD_TYPE \
+                -c tools.cmake.cmaketoolchain:generator=Ninja \
+                --build=missing \
+                --output-folder=$DESTINATION_DIR
+            source $DESTINATION_DIR/generators/conanbuild.sh
+            cmake -S . \
+                -B $DESTINATION_DIR \
+                --preset ${CONAN_PRESET[$BUILD_TYPE]}
+            source $DESTINATION_DIR/generators/deactivate_conanbuild.sh
+            ;;
+        "Windows")
+            conan install . \
+                -s build_type=$BUILD_TYPE \
+                -c tools.cmake.cmaketoolchain:generator=Ninja \
+                -c tools.microsoft.bash:subsystem=msys2 \
+                -c tools.microsoft.bash:active=True \
+                --build=missing \
+                --output-folder=$DESTINATION_DIR
+            source $DESTINATION_DIR/generators/conanbuild.sh
+            cmake -S . \
+                -B $DESTINATION_DIR \
+                --preset ${CONAN_PRESET[$BUILD_TYPE]}
+            source $DESTINATION_DIR/generators/deactivate_conanbuild.sh
+            ;;
+        "Android")
+            check_variable_existence ANDROID_NDK
+            check_variable_existence API_LEVEL
+            conan install . \
+                -s os=$OS \
+                -s arch=$ABI \
+                -s compiler=clang \
+                -s compiler.version=14 \
+                -c tools.cmake.cmaketoolchain:generator=Ninja \
+                -s os.api_level=$API_LEVEL \
+                -s build_type=$BUILD_TYPE \
+                -c tools.android:ndk_path=$ANDROID_NDK \
+                --build=missing \
+                --output-folder=$DESTINATION_DIR
+            source $DESTINATION_DIR/generators/conanbuild.sh
+            cmake -S . \
+                -B $DESTINATION_DIR \
+                --preset ${CONAN_PRESET[$BUILD_TYPE]}
+            source $DESTINATION_DIR/generators/deactivate_conanbuild.sh
+            ;;
+    esac
+fi
+
+# Build
+if ! [[ -n "$STAGE" ]] || [[ "$STAGE" == "Build" ]]; then
+    # Translations
+    while IFS= read -r lang
+    do
+        lang=$(echo "$lang" | tr -d '\r')
+        ${QT_HOST_PATH}/bin/lupdate ./src/ -ts ./translations/translation_${lang}.ts
+        ${QT_HOST_PATH}/bin/lrelease ./translations/*.ts
+    done < "./translations/list.txt"
+    mkdir -p ./rcc/rcc
+    mv ./translations/*.qm ./rcc/rcc
+
+    # Resources
+    ./rcc/rcc.sh
+
+    cmake --build $DESTINATION_DIR
+fi
 
 
-conan profile detect -f
-case $OS in
-    "Linux")
-        # Config
-        conan install . \
-            -s build_type=$BUILD_TYPE \
-            -c tools.cmake.cmaketoolchain:generator=Ninja \
-            --build=missing \
-            --output-folder=$DESTINATION_DIR
-        source $DESTINATION_DIR/generators/conanbuild.sh
-        cmake -S . \
-            -B $DESTINATION_DIR \
-            --preset ${CONAN_PRESET[$BUILD_TYPE]}
-        source $DESTINATION_DIR/generators/deactivate_conanbuild.sh
-
-        # Build
-        cmake --build $DESTINATION_DIR
-
-        # Deploy
-        cp ./android/res/drawable/icon.png ./$DESTINATION_DIR/${PROJECT_NAME}.png
-        cd ./$DESTINATION_DIR
-        linuxdeploy-x86_64.AppImage \
-            --appdir ./${PROJECT_NAME}.dir \
-            --executable ./${PROJECT_NAME} \
-            --icon-file ./${PROJECT_NAME}.png \
-            --create-desktop-file \
-            --output appimage \
-            --plugin qt
-        ;;
-    "Windows")
-        # Config
-        conan install . \
-            -s build_type=$BUILD_TYPE \
-            -c tools.cmake.cmaketoolchain:generator=Ninja \
-            -c tools.microsoft.bash:subsystem=msys2 \
-            -c tools.microsoft.bash:active=True \
-            --build=missing \
-            --output-folder=$DESTINATION_DIR
-        source $DESTINATION_DIR/generators/conanbuild.sh
-        cmake -S . \
-            -B $DESTINATION_DIR \
-            --preset ${CONAN_PRESET[$BUILD_TYPE]}
-        source $DESTINATION_DIR/generators/deactivate_conanbuild.sh
-
-        # Build
-        cmake --build $DESTINATION_DIR
-
-        # Deploy
-        mkdir $DESTINATION_DIR/${PROJECT_NAME}/
-        cp $DESTINATION_DIR/${PROJECT_NAME}.exe $DESTINATION_DIR/${PROJECT_NAME}/
-        windeployqt $DESTINATION_DIR/${PROJECT_NAME}/${PROJECT_NAME}.exe
-        tar -cjvf $DESTINATION_DIR/${PROJECT_NAME}.tar.bz2 $DESTINATION_DIR/${PROJECT_NAME}
-        ls -al $DESTINATION_DIR/
-        ;;
-    "Android")
-        check_variable_existence ANDROID_NDK
-        check_variable_existence API_LEVEL
-
-        # Config
-        conan install . \
-            -s os=$OS \
-            -s arch=$ABI \
-            -s compiler=clang \
-            -s compiler.version=14 \
-            -c tools.cmake.cmaketoolchain:generator=Ninja \
-            -s os.api_level=$API_LEVEL \
-            -s build_type=$BUILD_TYPE \
-            -c tools.android:ndk_path=$ANDROID_NDK \
-            --build=missing \
-            --output-folder=$DESTINATION_DIR
-        source $DESTINATION_DIR/generators/conanbuild.sh
-        cmake -S . \
-            -B $DESTINATION_DIR \
-            --preset ${CONAN_PRESET[$BUILD_TYPE]}
-        source $DESTINATION_DIR/generators/deactivate_conanbuild.sh
-
-        # Build
-        cmake --build $DESTINATION_DIR
-
-        # Deploy
-        if [ -n "$ANDROID_DEVICE_ID" ]; then
-            adb devices -l
-            adb -s $ANDROID_DEVICE_ID install $DESTINATION_DIR/android-build/*.apk
-        fi
-        ;;
-esac
+# Deploy
+if ! [[ -n "$STAGE" ]] || [[ "$STAGE" == "Deploy" ]]; then
+    case $OS in
+        "Linux")
+            cp ./android/res/drawable/icon.png ./$DESTINATION_DIR/${PROJECT_NAME}.png
+            cd ./$DESTINATION_DIR
+            export QMAKE=${Qt6_DIR}/bin/qmake
+            linuxdeploy-x86_64.AppImage \
+                --appdir ./${PROJECT_NAME}.dir \
+                --executable ./${PROJECT_NAME} \
+                --icon-file ./${PROJECT_NAME}.png \
+                --create-desktop-file \
+                --output appimage \
+                --plugin qt
+            ;;
+        "Windows")
+            mkdir $DESTINATION_DIR/${PROJECT_NAME}/
+            cp $DESTINATION_DIR/${PROJECT_NAME}.exe $DESTINATION_DIR/${PROJECT_NAME}/
+            windeployqt $DESTINATION_DIR/${PROJECT_NAME}/${PROJECT_NAME}.exe
+            tar -cjvf $DESTINATION_DIR/${PROJECT_NAME}.tar.bz2 $DESTINATION_DIR/${PROJECT_NAME}
+            ls -al $DESTINATION_DIR/
+            ;;
+        "Android")
+            if [ -n "$ANDROID_DEVICE_ID" ]; then
+                adb devices -l
+                adb -s $ANDROID_DEVICE_ID install $DESTINATION_DIR/android-build/*.apk
+            fi
+            ;;
+    esac
+fi
