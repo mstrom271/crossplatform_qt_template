e4f194b72ed0699abe54a2dbd193583837c4866c
Vitaliy Lytonin <mstrom2@rambler.ru>
2024-03-31 14:56:13 +0400

    add windows end of line adaptation for config.h.sh

--- config.h.sh
+++ config.h.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Reads variables from config.h in C format and adds it to the bash script environment
 
 FILE="src/config.h"
 
-while IFS=' ' read -r prefix key value
+while IFS= read -r line
 do
+    line=$(echo "$line" | tr -d '\r');      # windows end of line adaptation
+    read -r prefix key value <<< "$line"
     if [[ $prefix == "#define" ]]; then
-        value=${value//\"/}
+        value=${value//\"/}                 # delete ""
         export "$key"="$value"
     fi
 done < "$FILE"
